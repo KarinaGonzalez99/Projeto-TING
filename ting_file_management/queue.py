6bab3724177e6be7908389feb8983b0035fc58a8
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-07-24 12:18:18 -0300

    Requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,34 @@
+from collections import deque
 from ting_file_management.abstract_queue import AbstractQueue
 
 
 class Queue(AbstractQueue):
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.items = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        """Retorna o número de elementos na fila"""
+        return len(self.items)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        """Adiciona um item ao final da fila"""
+        self.items.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        """Remove um item do início da fila"""
+        if not self.is_empty():
+            return self.items.popleft()
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        """Retorna um item da fila baseado em um índice"""
+        if index < 0 or index >= len(self.items):
+            raise IndexError("Índice Inválido ou Inexistente")
+        else:
+            return self.items[index]
+
+    def is_empty(self):
+        """Retorna True se a fila estiver vazia"""
+        return not bool(self.items)
