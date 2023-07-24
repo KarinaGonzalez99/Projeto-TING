532dfd85b1216f3d914ed9c9c2ca3f2f7cccb567
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-07-24 12:31:10 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # Verifica se o arquivo existe
+    if not os.path.isfile(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
+
+    # Verifica se a extensão do arquivo é .txt
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    # Se tudo estiver ok, lê o arquivo e retorna uma lista com as linhas
+    with open(path_file, 'r', encoding='utf-8') as file:
+        lines = file.read().split('\n')
+
+    return lines
